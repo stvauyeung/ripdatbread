a304696d9b56728e8fbe754e9126124a10c213ff
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-28 08:41:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bread::Application.config.secret_key_base = 'fbbc9e05535bef0dff4f2cd1aadcde4546c505db4fff765d463950fdd4cb28fbc6a00d1fe5e8ee0370f5026343549434c274630e19452fddc015f8e879a28a83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bread::Application.config.session_store :cookie_store, key: '_bread_session'
