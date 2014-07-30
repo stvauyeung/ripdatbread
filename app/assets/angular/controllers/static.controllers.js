aa7cceda5ef5404e01820ba98ed9e76de6f546ff
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-29 19:30:21 -0700

    fixed overlay on landing

--- app/assets/angular/controllers/static.controllers.js
+++ app/assets/angular/controllers/static.controllers.js
@@ -3,7 +3,16 @@ controllers = angular.module('staticCtrls', []);
 controllers
   .controller('StaticHomeCtrl', ['$scope', function($scope) {
     $scope.breads = [];
-    for (var i = 0; i < 32; i++) {
+    for (var i = 0; i < 30; i++) {
       $scope.breads.push(angular.copy(bread1));
     };
+
+    // $scope.placeBackgroundImage = function(imgSrc) {
+    //   return {
+    //     'background-image': imgSrc,
+    //     'background-position': 'center top',
+    //     'background-repeat': 'no-repeat',
+    //     'background-size': '100% auto'
+    //   }
+    // }
   }]);
