c5b6a53a9c60f1e28bb68691df3827eacc6cacec
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-07-29 18:22:36 -0700

    put images on landing

--- app/assets/angular/controllers/static.controllers.js
+++ app/assets/angular/controllers/static.controllers.js
@@ -1,5 +1,9 @@
 controllers = angular.module('staticCtrls', []);
 
 controllers
-  .controller('StaticHomeCtrl', [
-    ]);
+  .controller('StaticHomeCtrl', ['$scope', function($scope) {
+    $scope.breads = [];
+    for (var i = 0; i < 32; i++) {
+      $scope.breads.push(angular.copy(bread1));
+    };
+  }]);
