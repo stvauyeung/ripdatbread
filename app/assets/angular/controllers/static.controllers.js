db7d9ff7551d154ff1b7e07dfa5246c4e1acae1c
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-08-02 17:33:17 -0700

    set up bread show, started user show

--- app/assets/angular/controllers/static.controllers.js
+++ app/assets/angular/controllers/static.controllers.js
@@ -10,6 +10,20 @@ controllers
   .controller('BreadShowCtrl', ['$scope', function($scope) {
     $scope.bread = angular.copy(bread1);
     $scope.onInfo = true;
+    $scope.breadInfoStyle = function() {
+      if ($scope.onInfo) {
+        return {'font-weight': '700'}
+      } else {
+        return {'font-weight': '300'}
+      };
+    };
+    $scope.breadTalkStyle = function() {
+      if ($scope.onInfo == false) {
+        return {'font-weight': '700'}
+      } else {
+        return {'font-weight': '300'}
+      };
+    };
   }])
   .controller('SideNavCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
     console.log($rootScope.hideSideNav);
@@ -17,4 +31,7 @@ controllers
       $rootScope.hideSideNav = true;
       console.log($rootScope.hideSideNav);
     }
+  }])
+  .controller('UserNavCtrl', ['$scope', function($scope) {
+    $scope.user = angular.copy(user);
   }]);
