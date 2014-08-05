68e71b8728a230e4263f1bc682f6a2b05123e834
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-08-04 22:29:49 -0700

    refactored menu toggle and comment button

--- app/assets/angular/controllers/static.controllers.js
+++ app/assets/angular/controllers/static.controllers.js
@@ -10,20 +10,6 @@ controllers
   .controller('BreadShowCtrl', ['$scope', function($scope) {
     $scope.bread = angular.copy(bread1);
     $scope.onInfo = true;
-    $scope.breadInfoStyle = function() {
-      if ($scope.onInfo) {
-        return {'font-weight': '700'}
-      } else {
-        return {'font-weight': '300'}
-      };
-    };
-    $scope.breadTalkStyle = function() {
-      if ($scope.onInfo == false) {
-        return {'font-weight': '700'}
-      } else {
-        return {'font-weight': '300'}
-      };
-    };
   }])
   .controller('SideNavCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
     console.log($rootScope.hideSideNav);
