65ceb9e9bc1e37f3cdd23fea9fc7274ad6520db6
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-08-06 21:56:54 -0700

    user create works

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::UsersController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+  
   def create
-      
+    user = User.create(user_params)
+    render nothing: true
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
