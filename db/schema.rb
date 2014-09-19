48dfcd32f98a414d49cb6caee45020715da57856
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-09-18 19:38:38 -0700

    reset db and removed images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827020811) do
+ActiveRecord::Schema.define(version: 20140919022706) do
 
   create_table "breads", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140827020811) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "photo"
+    t.integer  "votes_count", default: 0
   end
 
   create_table "comments", force: true do |t|
